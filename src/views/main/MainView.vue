eb8ee37cfce0e3c41a525fdfd1e98e0f84b7e337
lyjoon <lyjoon@gmail.com>
2023-08-08 00:04:36 +0900

    123

---
+++ src/views/main/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>환불해주세요.</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class MainView extends Vue {
+  protected mounted() {
+    console.log("MAIN10000");
+  }
+}
+</script>
